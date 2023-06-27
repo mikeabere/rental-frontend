189fe6709642c801f297383918b23c5f4a4c896b
mike mwanzi <mikeabere@gmail.com>
2023-06-27 12:03:27 +0300

    added header

---
+++ rental-client/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function Header(){
+
+    return(
+        <>
+         <header>
+            <h1>Smart Hama</h1>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Houses</li>
+                <li>Contacts</li>
+            </nav>
+         </header>
+        </>
+    )
+}
